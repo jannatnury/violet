e8c4a65b7b735fce1b0f1d67a263a0d9f4834ffb
jannatnury <jannatnury253@gmail.com>
2022-05-13 23:49:25 +0600

    feedback added

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Feedback.css';
+import img from '../../assets/images/emails.svg';
+// import { toast } from 'toastify-react-native';
+
+
+const Feedback = () => {
+    //  const notifySuccess = (message) => {
+    //         toast.success(message, {
+    //             theme: "dark",
+    //             position: toast.POSITION.TOP_CENTER
+    //         });
+    //     }
+    return (
+        <div className='mt-5 shadow container p-4 mb-5 feedback-sec m-auto'>
+            <h2 className='mt-3'>Give Your Feedback</h2>
+            <div className='row container my-4 order-sm-last m-auto align-items-center'>
+                <div className="col-lg-6 ">
+                    <input className='p-3 my-3 shadow' type="email" name="email" id="email-input" placeholder='Enter your email' required /> <br />
+                    <input className='p-3 shadow' type="text" placeholder='Write your comment' id="comments" required /> <br />
+                    <button className='feedback-btn p-2 m-2' type="submit">Give Feedback</button>
+                </div>
+                <div className="col-lg-6 feedback">
+                    <img src={img} alt="" />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Feedback;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import ChooseUs from '../ChooseUs/ChooseUs';
+import Feedback from '../Feedback/Feedback';
 import './Home.css';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <ChooseUs></ChooseUs>
+            <Feedback></Feedback>
         </div>
     );
 };
