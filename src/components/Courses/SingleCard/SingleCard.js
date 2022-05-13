09954680b7a13ba9e7502a3453d2c98a150efb7f
jannatnury <jannatnury253@gmail.com>
2022-05-14 00:30:45 +0600

    cards edited

--- src/components/Courses/SingleCard/SingleCard.js
+++ src/components/Courses/SingleCard/SingleCard.js
@@ -5,8 +5,8 @@ const SingleCard = ({ card }) => {
     const { img, name, price, details } = card;
 
     return (
-        <div className='col-lg-4 col-sm-12 mt-3 m-auto'>
-            <div className="card  shadow" style={{ width: "18rem" }}>
+        <div className='col-lg-4 col-sm-12 mt-3'>
+            <div className="card  shadow m-autu" style={{ width: "18rem" }}>
                 <img src={img} className="card-img-top" alt="card image" />
                 <div className="card-body">
                     <h5 className="card-title text-secondary font-monospace">{name}</h5>
